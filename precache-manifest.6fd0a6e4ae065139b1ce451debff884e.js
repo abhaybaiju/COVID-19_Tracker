c6927957a06e0551cc945c18535985faa04791e2
abhay.baiju <abhay0198@gmail.com>
2020-06-15 07:03:37 -0700

    Updates

---
+++ precache-manifest.6fd0a6e4ae065139b1ce451debff884e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3538c53b7f794e4a6310db14dee55687",
+    "url": "/COVID-19_Tracker/index.html"
+  },
+  {
+    "revision": "0bd2fb1c2f1f52514588",
+    "url": "/COVID-19_Tracker/static/css/main.e2dc94c1.chunk.css"
+  },
+  {
+    "revision": "55ba9860113c15b6d9e5",
+    "url": "/COVID-19_Tracker/static/js/2.c79fc9c5.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID-19_Tracker/static/js/2.c79fc9c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bd2fb1c2f1f52514588",
+    "url": "/COVID-19_Tracker/static/js/main.2a0dfec4.chunk.js"
+  },
+  {
+    "revision": "08eb81572a38520882aa",
+    "url": "/COVID-19_Tracker/static/js/runtime-main.906a52a5.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID-19_Tracker/static/media/image.d7265326.png"
+  }
+]);
